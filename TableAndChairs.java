10298cdefdaca6f53f2e2063144d644dff3ff740
jamesmawunyo82-jpg <jamesmawunyo82@gmail.com>
2026-06-11 17:48:05 +0000

    Add files via upload

---
+++ TableAndChairs.java
@@ -0,0 +1,11 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X           X");
+        System.out.println("X           X");
+        System.out.println("X           X");
+        System.out.println("XXXXX XXXXXXXXX XXXXX");
+        System.out.println("  X   X       X   X");
+        System.out.println("  X   X       X   X");
+        System.out.println("  X   X       X   X");
+    }
+}
